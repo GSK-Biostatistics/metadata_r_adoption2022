13bcabe308411dd56a2e7b2c5a6c8c571270b28b
Christina Fillmore <55274484+statasaurus@users.noreply.github.com>
2022-09-29 14:53:08 +0100

    Update code for tansitions

--- Example.R
+++ Example.R
@@ -4,14 +4,13 @@ library(haven)
 library(dplyr)
 
 # Read in the metadata 
-metacore<- spec_to_metacore("specs.xlsx", where_sep_sheet = FALSE)
+metacore<- spec_to_metacore("specs.xlsx", where_sep_sheet = FALSE, quiet = TRUE)
 
 # Read in datasets as normal
 dm <- read_xpt("SDTM/dm.xpt")
 
 print(metacore)
 
-metacore$ds_spec
 # It has the 3 datasets we will be using for our tables 
 metacore$ds_spec
 # We can see the metadata from the spec document is now in this structure in R 
@@ -21,11 +20,15 @@ head(metacore$var_spec)
 adsl_spec <- metacore %>% 
   select_dataset("ADSL")
 
+check_variables(tibble(), adsl_spec)
+
 # the next step is to pull in the predecessor variables 
 adsl_pred <- build_from_derived(adsl_spec, 
                                 ds_list = list("dm" = dm), 
                                 keep = TRUE) %>% 
   filter(ARMCD %in% c("A", "P"))
+
+check_variables(adsl_pred, adsl_spec)
 
 # Using the metacore object we can make the decodes of control terminology 
 get_control_term(adsl_spec, variable = SEXN)
@@ -36,7 +39,7 @@ adsl_pred %>%
 
 # Now we can repeat the process for all our code/decode pairs 
 adsl_decode<- adsl_pred %>% 
-  create_var_from_codelist(adsl_spec, input_var = SEX, out_var = SEXN)%>% 
+  create_var_from_codelist(adsl_spec, input_var = SEX, out_var = SEXN) %>% 
   create_var_from_codelist(adsl_spec, input_var = ETHNIC, out_var = ETHNICN) %>% 
   create_var_from_codelist(adsl_spec, input_var = ARMCD, out_var = TRT01PN) %>% 
   create_var_from_codelist(adsl_spec, input_var = ACTARMCD, out_var = TRT01AN) %>%
@@ -55,7 +58,7 @@ adsl_decode %>%
 adsl_grouped <- adsl_decode %>%
   create_cat_var(adsl_spec, ref_var = AGE, grp_var = AGEGR1, num_grp_var = AGEGR1N) 
 
-
+check_variables(adsl_grouped, adsl_spec)
 # Once you have created all the variables will want to add label 
 adsl <- adsl_grouped %>% 
   order_cols(adsl_spec) %>%
